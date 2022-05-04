1043655da03bbd0bde744c025112cc6f95148d7e
ofirNakdai <93441082+ofirNakdai@users.noreply.github.com>
2022-05-04 13:09:31 +0300

    Q2.4

--- ex2-pandas.py
+++ ex2-pandas.py
@@ -1,15 +1,25 @@
+from cmath import sqrt
 import pandas as pd
 import string
 
 #Q2.2----------------------------------------------------
 def reindex_up_down(s):
-    
     temp = pd.Series(s.index)
     low = list(string.ascii_lowercase)
-
 
     mask = pd.Series(temp.str.startswith(tuple(low)))
     temp = temp.where((mask.values),temp.str.upper())
     temp = temp.where((mask.values == False),temp.str.lower())
     s.index = temp.values
+    
     return temp
+
+#Q2.4----------------------------------------------------
+def partial_sum(s):
+    res = s.abs()
+    res = res.sum()
+    return sqrt(res)
+
+
+
+
