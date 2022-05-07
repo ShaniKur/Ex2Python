6289b2b5c59d21a5cdf778c2f4227a0eb3595e46
ofirNakdai <93441082+ofirNakdai@users.noreply.github.com>
2022-05-07 13:48:40 +0300

    Question 2.10

--- ex2-pandas.py
+++ ex2-pandas.py
@@ -30,5 +30,55 @@ def dropna_mta_style(df, how= "any" ):
 
 
 #Q2.8----------------------------------------------------
+                    #Doesn't Work!!!
 
+def unique_dict(df, how="col"):
+    
+    if how.lower() == "col":
+        frame = df.T
+    else:
+        frame = df
+        
+    mask = frame.duplicated()
+    res = pd.DataFrame(frame).where(mask != False,other = NaN)
+    res = res.dropna()
+
+    ans = {}
+    for i in res.index:
+        ans[i] = pd.Series(res.loc[i].values)
+        
+    return ans
+
+
+data = {"age":[12,12,12,12],
+        "name": ["ofir","amit","nofar","nofar"]}
+
+frame = pd.DataFrame(data, index=['a','b','c','d'])
+
+
+mask = frame.duplicated(keep=False)
+res = pd.DataFrame(frame).where(mask == False,other = NaN)
+res = res.dropna()
+
+print(mask)
+print(res)
+
+#Q2.10----------------------------------------------------
+def is_stable(marriage, men, wemen):
+    res = True
+    man = marriage[1];
+    woman = marriage[0];
+
+    if (men.loc[man][0] != woman):
+        res = False
+    
+    if (wemen.loc[woman][0] != man):
+        res = False    
+        
+    return res
+    
+
+def stable_marriage(dames, gents, marriages):
+    mask = [is_stable(x,gents, dames) for x in marriages]
+    return all(mask)
 
